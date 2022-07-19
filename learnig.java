69bae49da89c65d5aae29bbb2aa0a04a98ac9ec5
Paresh Vaghasiya <pv20aai@herts.ac.uk>
2022-07-19 20:55:23 +0100

    my first git program

---
+++ learnig.java
@@ -0,0 +1,54 @@
+package main.java;
+
+public class learnig {
+    public static void main(String[] args) {
+// Using integer data type
+           int num  = 52;
+           int num2  = 55;
+           short sh  = 34;
+           byte by = 44;
+           long lg = 98;
+        System.out.println("integer data type" + "\n"  +num + num2);
+        System.out.println(num + sh);
+        System.out.println(by + sh);
+        System.out.println(lg + by);
+        System.out.println("--------------------------------");
+
+ //float data type
+        float fl = 33.33f;
+        float lt = 55.34f;
+        System.out.println("float data type"+"\n"+fl + lt);
+        System.out.println(fl - lt);
+        System.out.println(fl / lt);
+        System.out.println(fl * lt);
+        System.out.println("--------------------------------");
+
+
+// character  data type
+        char ch = 2;
+        char dc = 5;
+        System.out.println("character data type"+"\n"+ch + dc);
+        System.out.println(ch - dc);
+        System.out.println("--------------------------------");
+
+
+// all arithmetic operation
+        int data = 34;
+        int data2 = 60;
+        int sum 		= data + data2;
+        int difference 	= data - data2;
+        int product 	= data * data2;
+        int quotient	= data / data2;
+        int modulo	    = data % data2;
+        System.out.println("perform all arithmetic operation");
+
+        System.out.println(sum);
+        System.out.println(difference);
+        System.out.println(product);
+        System.out.println(quotient);
+        System.out.println(modulo);
+
+
+
+    }
+}
